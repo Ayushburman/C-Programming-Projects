7b709132e72e074ee28aa38ac7e4be9548237c53
Ayush Burman <72551504+Ayushburman@users.noreply.github.com>
2026-05-03 23:02:32 +0530

    Update ProteinCalculator.c

--- ProteinCalculator.c
+++ ProteinCalculator.c
@@ -1,13 +1,3 @@
 
 
-🔹 STEP 6: Calculate Protein Requirement
-IF activity = 1 → protein_need = 0.8 × weight
-ELSE IF activity = 2 → protein_need = 1.0 × weight
-ELSE IF activity = 3 → protein_need = 1.2 × weight
-ELSE IF activity = 4 → protein_need = 1.5 × weight
-ELSE:
-Print "Invalid activity"
-STOP program
-  
-
 

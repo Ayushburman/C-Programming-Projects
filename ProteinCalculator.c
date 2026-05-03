82c7e13880034fd6591c5b0b1966b30203e41221
Ayush Burman <72551504+Ayushburman@users.noreply.github.com>
2026-05-03 22:08:01 +0530

    Update ProteinCalculator.c

--- ProteinCalculator.c
+++ ProteinCalculator.c
@@ -1,2 +1,11 @@
 #ifndef PROTEIN_H
 #define PROTEIN_H
+// ─────────────────────────────────────────
+//  Diet types
+// ─────────────────────────────────────────
+typedef enum {
+    DIET_VEG        = 1,
+    DIET_NONVEG     = 2,
+    DIET_VEGAN      = 3,
+    DIET_EGGETARIAN = 4
+} DietType;

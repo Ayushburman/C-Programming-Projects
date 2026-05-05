4d4f4b1bf659bca45e25d19a2e6fde1efe2bf2c2
Ayush Burman <72551504+Ayushburman@users.noreply.github.com>
2026-05-05 22:27:29 +0530

    Update UrineAnalysisProgram.c

--- UrineAnalysisProgram.c
+++ UrineAnalysisProgram.c
@@ -7,3 +7,10 @@ int main() {
     printf("===== URINE ANALYSIS SYSTEM =====\n");
     printf("Answer the following questions:\n\n");
 
+ // Q1: Color
+    printf("1. What is your urine color?\n");
+    printf("1. Pale Yellow (Normal)\n");
+    printf("2. Dark Yellow\n");
+    printf("3. Red / Brown\n");
+    printf("Enter choice: ");
+    scanf("%d", &choice);

0329177bdb0c95272a9971193e6502883f16d4b2
Ayush Burman <72551504+Ayushburman@users.noreply.github.com>
2026-05-05 22:01:35 +0530

    Update HogwartsHouseSorting.c

--- HogwartsHouseSorting.c
+++ HogwartsHouseSorting.c
@@ -107,3 +107,7 @@ int main() {
     else if(choice == 2) score[SLYTHERIN]+=3;
     else if(choice == 3) score[RAVENCLAW]+=3;
     else score[HUFFLEPUFF]+=3;
+ // Q4
+    printf("\nQ4: Choose a magical subject:\n");
+    printf("1. Defense Against Dark Arts\n2. Potions\n3. Astronomy\n4. Herbology\n");
+    scanf("%d", &choice);

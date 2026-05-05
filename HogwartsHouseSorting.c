7e1e4784a7a620d8506d41f1e0c70f103cdf6870
Ayush Burman <72551504+Ayushburman@users.noreply.github.com>
2026-05-05 22:04:02 +0530

    Update HogwartsHouseSorting.c

--- HogwartsHouseSorting.c
+++ HogwartsHouseSorting.c
@@ -6,6 +6,7 @@
 #define SLYTHERIN 1
 #define RAVENCLAW 2
 #define HUFFLEPUFF 3
+
 // Function to find max index
 int maxIndex(int arr[]) {
     int max = arr[0], idx = 0;
@@ -13,16 +14,17 @@ int maxIndex(int arr[]) {
         if(arr[i] > max) {
             max = arr[i];
             idx = i;
-  }
+        }
     }
     return idx;
 }
+
 // Wand selection
 void getWand(int house) {
     printf("\n--- Your Wand ---\n");
     switch(house) {
         case GRYFFINDOR:
-  printf("Core: Phoenix Feather\nWood: Holly\nLength: 11 inches\n");
+            printf("Core: Phoenix Feather\nWood: Holly\nLength: 11 inches\n");
             break;
         case SLYTHERIN:
             printf("Core: Dragon Heartstring\nWood: Yew\nLength: 12.5 inches\n");
@@ -35,6 +37,7 @@ void getWand(int house) {
             break;
     }
 }
+
 // Pet selection
 void getPet(int house) {
     printf("\n--- Your Magical Pet ---\n");
@@ -53,6 +56,7 @@ void getPet(int house) {
             break;
     }
 }
+
 // Patronus
 void getPatronus(int house) {
     printf("\n--- Your Patronus ---\n");
@@ -71,6 +75,7 @@ void getPatronus(int house) {
             break;
     }
 }
+
 int main() {
 
     int score[4] = {0};
@@ -79,7 +84,8 @@ int main() {
     printf("====================================\n");
     printf("   Welcome to the Hogwarts Quiz\n");
     printf("====================================\n");
- // Q1
+
+    // Q1
     printf("\nQ1: You find a locked ancient door. What do you do?\n");
     printf("1. Break it open\n2. Find hidden mechanism\n3. Analyze runes\n4. Knock politely\n");
     scanf("%d", &choice);
@@ -88,7 +94,8 @@ int main() {
     else if(choice == 2) { score[SLYTHERIN]+=3; score[RAVENCLAW]+=1; }
     else if(choice == 3) { score[RAVENCLAW]+=3; }
     else { score[HUFFLEPUFF]+=3; }
- // Q2
+
+    // Q2
     printf("\nQ2: A friend cheats in exam. You...\n");
     printf("1. Confront\n2. Ignore\n3. Understand reason\n4. Help them improve later\n");
     scanf("%d", &choice);
@@ -98,7 +105,7 @@ int main() {
     else if(choice == 3) score[RAVENCLAW]+=3;
     else score[HUFFLEPUFF]+=3;
 
-// Q3
+    // Q3
     printf("\nQ3: What motivates you most?\n");
     printf("1. Glory\n2. Power\n3. Knowledge\n4. Loyalty\n");
     scanf("%d", &choice);
@@ -107,7 +114,8 @@ int main() {
     else if(choice == 2) score[SLYTHERIN]+=3;
     else if(choice == 3) score[RAVENCLAW]+=3;
     else score[HUFFLEPUFF]+=3;
- // Q4
+
+    // Q4
     printf("\nQ4: Choose a magical subject:\n");
     printf("1. Defense Against Dark Arts\n2. Potions\n3. Astronomy\n4. Herbology\n");
     scanf("%d", &choice);
@@ -117,18 +125,18 @@ int main() {
     else if(choice == 3) score[RAVENCLAW]+=3;
     else score[HUFFLEPUFF]+=3;
 
-// Q5
+    // Q5
     printf("\nQ5: You see a creature in danger:\n");
     printf("1. Jump in immediately\n2. Plan rescue\n3. Study first\n4. Call others\n");
     scanf("%d", &choice);
-
 
     if(choice == 1) score[GRYFFINDOR]+=3;
     else if(choice == 2) score[SLYTHERIN]+=2;
     else if(choice == 3) score[RAVENCLAW]+=3;
     else score[HUFFLEPUFF]+=3;
 
- int house = maxIndex(score);
+    // Result
+    int house = maxIndex(score);
 
     printf("\n====================================\n");
     printf("         Your Results\n");

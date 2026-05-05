3fa64e0b71b653fc62e5970f3de2d6d5b437c4c0
Ayush Burman <72551504+Ayushburman@users.noreply.github.com>
2026-05-05 21:56:00 +0530

    Update HogwartsHouseSorting.c

--- HogwartsHouseSorting.c
+++ HogwartsHouseSorting.c
@@ -6,3 +6,10 @@
 #define SLYTHERIN 1
 #define RAVENCLAW 2
 #define HUFFLEPUFF 3
+// Function to find max index
+int maxIndex(int arr[]) {
+    int max = arr[0], idx = 0;
+    for(int i = 1; i < 4; i++) {
+        if(arr[i] > max) {
+            max = arr[i];
+            idx = i;
